3394de1d8ed3bb665c4bc1dbb707d1cd2448899b
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-01-02 19:12:04 -0500

    create stella module

---
+++ stella/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup_info = dict(
+    name='stella',
+    version='0.0.1',
+    author='usymmij',
+    author_email='usymmij@gmail.com',
+    description='autonomous agent in CARLA',
+    license='MIT',
+)
+
+
+setup(**setup_info)
