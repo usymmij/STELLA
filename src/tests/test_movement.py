2df5a7b4a62b9427317e9c05d09b00786a3a32e5
Madelyn Hoffman <mhoffm32@uwo.ca>
2025-02-11 15:15:40 -0500

    proper CARLAInterface imports

--- src/tests/test_movement.py
+++ src/tests/test_movement.py
@@ -1,10 +1,15 @@
+# To run this file: "python -m tests.test_movement" from src directory
 import time
-from CARLA_movement_interface import CARLAInterface
+import sys
+import os
+import pygame
+
+from modules.movement import CARLAInterface
 
 if __name__ == "__main__":
+    
     interface = CARLAInterface()
     interface.spawn_vehicle()
-
     time.sleep(2)
     print("Moving forward...")
     interface.move_forward(throttle=0.7)
@@ -23,4 +28,32 @@ if __name__ == "__main__":
 
     status = interface.get_status()
     print("Vehicle Status:", status)
+
+
+
+
+
+#from modules.movement import CARLAInterface
+
+# if __name__ == "__main__":
     
+#     interface = CARLAInterface()
+#     interface.spawn_vehicle()
+#     time.sleep(2)
+#     print("Moving forward...")
+#     interface.move_forward(throttle=0.7)
+#     time.sleep(5)
+
+#     print("Slamming brakes...")
+#     interface.brake(1.0)
+#     time.sleep(5)
+    
+#     print("Moving forward...")
+#     interface.move_forward(throttle=0.1)
+#     time.sleep(5)
+
+#     print("Stopping...")
+#     interface.stop()
+
+#     status = interface.get_status()
+#     print("Vehicle Status:", status)
