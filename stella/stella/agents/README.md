915ef633ee4e7f35788e23f45d178efe6d9f009c
usymmij <usymmij@gmail.com>
2025-02-12 16:33:23 -0500

    change transfuser launch script

--- stella/stella/agents/README.md
+++
@@ -1,7 +0,0 @@
-# Agents
-
-- PID tools taken from the CARLA PythonAPI
-
-Copyright (c) 2024 Computer Vision Center (CVC) at the Universitat Autonoma de
-Barcelona (UAB).
-
--- stella/tests/test_movement.py
+++ stella/tests/test_movement.py
@@ -1,19 +1,20 @@
-import pytest
+# To run this file: "python -m tests.test_movement" from src directory
 import time
+import sys
+import os
+import pygame
 
-"""what is CARLA_movement_interface?? doesn't show up in any docs -usymmij"""
-# from CARLA_movement_interface import CARLAInterface
- 
-@pytest.mark.skip(reason="CARLA_movement_interface fails plz fix @dbazinag")
-def test_movement():
+from modules.movement import CARLAInterface
+
+if __name__ == "__main__":
+    
     interface = CARLAInterface()
     interface.spawn_vehicle()
-    
     time.sleep(2)
     print("Moving forward...")
     interface.move_forward(throttle=0.7)
     time.sleep(5)
-    
+
     print("Slamming brakes...")
     interface.brake(1.0)
     time.sleep(5)
@@ -21,10 +22,38 @@ def test_movement():
     print("Moving forward...")
     interface.move_forward(throttle=0.1)
     time.sleep(5)
-    
+
     print("Stopping...")
     interface.stop()
-    
+
     status = interface.get_status()
     print("Vehicle Status:", status)
+
+
+
+
+
+#from modules.movement import CARLAInterface
+
+# if __name__ == "__main__":
     
+#     interface = CARLAInterface()
+#     interface.spawn_vehicle()
+#     time.sleep(2)
+#     print("Moving forward...")
+#     interface.move_forward(throttle=0.7)
+#     time.sleep(5)
+
+#     print("Slamming brakes...")
+#     interface.brake(1.0)
+#     time.sleep(5)
+    
+#     print("Moving forward...")
+#     interface.move_forward(throttle=0.1)
+#     time.sleep(5)
+
+#     print("Stopping...")
+#     interface.stop()
+
+#     status = interface.get_status()
+#     print("Vehicle Status:", status)
