1ec2728a6a530244edbeeceb3cf3f5284b82f107
Dominic Bazina-Grolinger <dbazinag@uwo.ca>
2025-02-12 15:17:23 -0500

    FEAT: Added gps processing and storing

--- stella/stella/sensors.py
+++ stella/stella/sensors.py
@@ -4,12 +4,12 @@ class Sensors:
     def __init__(self, world, vehicle):
         self.world = world
         self.vehicle = vehicle
-        self.sensors = [] # to keep track of attached sensors (can remove if we want just good for cleanup)
+        self.sensors = []
 
     def add_gps_sensor(self):
         blueprint_library = self.world.get_blueprint_library()
         gps_bp = blueprint_library.find('sensor.other.gnss')
-        spawn_point = carla.Transform(carla.Location(x=0, z=2.0)) # can adjust position if we need
+        spawn_point = carla.Transform(carla.Location(x=0, z=2.0))
         gps_sensor = self.world.spawn_actor(gps_bp, spawn_point, attach_to=self.vehicle)
         gps_sensor.listen(self.process_gps_data)
         self.sensors.append(gps_sensor)
@@ -17,5 +17,13 @@ class Sensors:
         return gps_sensor
     
     def process_gps_data(self, gps):
-        print(f"GPS Data: Latitude {gps.latitude}, Longitude {gps.longitude}, Altitude {gps.altitude}")
-        # we can save or log this data to a file or something if we want?
+        gps_entry = {
+            "latitude": gps.latitude,
+            "longitude": gps.longitude,
+            "altitude": gps.altitude
+        }
+        self.gps_data.append(gps_entry)
+        print(f"Collected GPS Data: {gps_entry}")
+
+    def get_collected_gps_data(self):
+        return self.gps_data
