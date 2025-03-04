4f84138692e694f22c83063bdf56300a4ea24d3c
Madelyn Hoffman <mhoffm32@uwo.ca>
2025-03-04 11:05:18 -0500

    Vit

--- src/tests/test_movement.py
+++ src/tests/test_movement.py
@@ -30,30 +30,3 @@ if __name__ == "__main__":
     print("Vehicle Status:", status)
 
 
-
-
-
-#from modules.movement import CARLAInterface
-
-# if __name__ == "__main__":
-    
-#     interface = CARLAInterface()
-#     interface.spawn_vehicle()
-#     time.sleep(2)
-#     print("Moving forward...")
-#     interface.move_forward(throttle=0.7)
-#     time.sleep(5)
-
-#     print("Slamming brakes...")
-#     interface.brake(1.0)
-#     time.sleep(5)
-    
-#     print("Moving forward...")
-#     interface.move_forward(throttle=0.1)
-#     time.sleep(5)
-
-#     print("Stopping...")
-#     interface.stop()
-
-#     status = interface.get_status()
-#     print("Vehicle Status:", status)
